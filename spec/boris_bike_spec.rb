67d22081ef9affc93f560046f765baa44417e5dd
Aram Simonian <aram.contacts@gmail.com>
2017-09-26 16:14:54 +0100

    Challenges 2-6

---
+++ spec/boris_bike_spec.rb
@@ -0,0 +1,7 @@
+require "DockingStation"
+
+describe DockingStation do
+#  it 'returns an object of type DockingStation when executed' do
+#    expect(docking_station = DockingStation.new).to eq DockingStation
+#  end
+end
