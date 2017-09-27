d6788713b48e63fc4a929a12459949f506715a78
mormolis <george.lamprakis@gmail.com>
2017-09-27 16:53:41 +0100

    creates an instance variable that stores the docked bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,9 +2,17 @@
 require File.dirname(__FILE__) + "/bike" 
 
 class DockingStation
+  attr_reader :bikes
+  def initialize
+    @bikes=[]
+  end
 
   def release_bike
     bike = Bike.new
+  end
+
+  def dock_bike(bike)
+    
   end
 
 end
