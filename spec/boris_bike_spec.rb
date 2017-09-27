2c400b62146ebaaf5c8ed2bb2ca2d6f195a39c24
Aram Simonian <aram.contacts@gmail.com>
2017-09-27 12:16:57 +0100

    includes challenge 9

--- spec/boris_bike_spec.rb
+++ spec/boris_bike_spec.rb
@@ -1,7 +1,27 @@
 require "DockingStation"
+require "bike"
 
 describe DockingStation do
-#  it 'returns an object of type DockingStation when executed' do
-#    expect(docking_station = DockingStation.new).to eq DockingStation
-#  end
+
+  describe ".release_bike" do
+    context "given the method 'release bike'"
+      it 'responds to the method "release_bike"' do
+        expect(subject).to respond_to :release_bike
+      end
+
+    context "release_bike returns object of type Bike"
+      it 'returns object of type Bike' do
+        expect(subject.release_bike).to be_instance_of Bike
+      end
+
+  end
+end
+
+describe Bike do
+  describe "working?" do
+    it 'responds to the method "working?"' do
+      expect(subject).to respond_to :working?
+    end
+
+  end
 end
